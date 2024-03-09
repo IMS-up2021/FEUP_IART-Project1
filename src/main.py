039e3fe3fa06b4f3ec009e796baa3198b5f00e8b
Inês Soares <ines.soares.22102003@gmail.com>
2024-03-09 11:44:29 +0000

    First version of main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from levels import levels
 
 if name == "main":
-    for level in LEVELS:
+    for level in levels:
         # Create the game for the current level
         game = draw.Game(level)
         
