d1a969e635a0eeae41879ebf9c6af020ae90c9cb
Inês <ines.soares.22102003@gmail.com>
2024-03-14 16:14:09 +0000

    Fixed main.py

--- src/main.py
+++ src/main.py
@@ -1,13 +1,7 @@
-import menu
+from menu import main_menu
 from levels import LEVELS
 
 
 
 if __name__ == "__main__":
-    for level in LEVELS:
-        # Create the game for the current level
-        game = draw.Game(level)
-        
-        # Run the game
-        if game.run():
-            break
+    main_menu()
